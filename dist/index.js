d7d6823bd319c2f27b5e116832a38585a442cdca
yutingrui <yutingrui@tal>
2026-05-18 10:35:56 +0800

    refactor: rename _tmpN → _tmp (scoped, no counter needed)

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@ import { emitFile } from "@typespec/compiler";
 import { collectServices, extractFields, scalarName, isArrayType, isRecordType, arrayElementType, recordElementType, toSnakeCase, dottedPathToSnakeCase, checkAndReportReservedKeywords, } from "@specodec/typespec-emitter-core";
 let _tmpCounter = 0;
 function nextTmp() {
-    return `$_tmp${_tmpCounter++}`;
+    return `$_tmp`;
 }
 function fieldPhp(name) {
     return toSnakeCase(name); // safeFieldName("php", ...) when emitter-core supports it
